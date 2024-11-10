dd2e7e0f46bb54c68b339d009c3146cd6c4e648e
iamveerendragangwar <128514719+helovirendra40@users.noreply.github.com>
2024-11-11 01:13:12 +0530

    Update Hero.jsx

--- fontend/src/components/Hero.jsx
+++ fontend/src/components/Hero.jsx
@@ -9,7 +9,6 @@ import { IoMdDownload } from "react-icons/io";
 import resume from '../assets/resume.pdf'
 
 import { saveAs } from "file-saver";
-import { Link } from "react-router-dom";
 
 
   
@@ -57,8 +56,7 @@ const Hero = () => {
                 <div className="heroButtons">
                     <a className="btn theme-btn-1" href="#contact">CONTACT ME <RiContactsFill /></a>
                     <button onClick={downloadPdf} className="btn theme-btn-2">GET RESUME
-                    <IoMdDownload /> vv</button>
-        <Link to={'/dashboard-skills'}>Add Skills</Link>
+                    <IoMdDownload /> </button>
                 </div>
 
 
